dcfbdbaec26c48df4d15ca9b2ebc240dfbc491bd
gkreative23-ship-it <gkreative23@gmail.com>
2026-06-20 19:18:18 +0100

    initial commit

---
+++ projects.js
@@ -0,0 +1,17 @@
+window.onload = function checkThemeChange(){
+    const savedTheme = localStorage.getItem('websiteTheme');
+
+    if(savedTheme=== 'purple-theme'){
+        document.documentElement.classList.add('girly-theme');
+    }
+};
+
+function changeTheme(){
+     document.documentElement.classList.toggle('girly-theme');
+
+     if(document.documentElement.classList.contains('girly-theme')) {
+        localStorage.setItem('websiteTheme', 'purple-theme');
+     } else{
+        localStorage.setItem('websiteTheme', 'default')
+     }
+}
